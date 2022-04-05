fef2dc753461f073ae4ae7846555a7b63b2c879c
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-04-05 17:24:21 +0200

    Switch case

---
+++ switch_case.js
@@ -0,0 +1,37 @@
+// let number = 20;
+
+/*let page = 1;
+let action = "go to next page";
+
+switch (action) {
+    case "go to next page":
+        return page + 1;
+    case "go to prev page":
+        return page - 1;
+    default:
+        console.log("Action not recognized!");
+        break;
+}
+*/
+  switch (number) {
+       case 20:
+         console.log("is 20!");
+         break;
+       case 100:
+         console.log("is 100!");
+         break;
+       case 10:
+          console.log("bingo!");
+          break;
+         default:
+          console.log("never mind!");
+         break;
+ }
+
+// if (number === 20) {
+//     console.log("is 20!");
+// } else if (number === 100) {
+//     // ...
+// } else {
+//     console.log("never mind!");
+// }
