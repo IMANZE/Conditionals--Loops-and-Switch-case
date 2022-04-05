2c4282b203dc72df27417bef4f0ae43760170012
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-04-05 17:20:51 +0200

    Loops

---
+++ loops.js
@@ -0,0 +1,26 @@
+/*for (let i = 0; i < 10; i ++) {
+    if(i % 2 === 0) {
+        continue;
+    }
+
+    console.log(i);
+}
+*/
+// console.log("broke out");
+
+// 5 % 2 === 1
+
+/*let i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i ++;
+
+}
+*/
+let i = 10;
+
+do {
+    console.log(i);
+    i ++;
+} while (i < 10);
